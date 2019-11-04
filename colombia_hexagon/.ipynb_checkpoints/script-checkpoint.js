6e16820c94e6b56d42058e1307752afd5accd2dc
colombiaendato <col.en.datos@gmail.com>
2019-11-04 18:13:49 -0500

    30DMC4D

---
+++ colombia_hexagon/.ipynb_checkpoints/script-checkpoint.js
@@ -0,0 +1,24 @@
+var vis = d3.select("body").append("svg")
+         .attr("width", 1000)
+         .attr("height", 667),
+
+scaleX = d3.scaleLinear()
+        .domain([-30,30])
+        .range([0,600]),
+
+scaleY = d3.scaleLinear()
+        .domain([0,50])
+        .range([500,0]),
+
+poly = [{"x":0.0, "y":25.0},
+        {"x":8.5,"y":23.4},
+        {"x":13.0,"y":21.0},
+        {"x":19.0,"y":15.5}];
+
+vis.selectAll("polygon")
+    .data([poly])
+    .enter().append("polygon")
+    .attr("points",function(d) { 
+        return d.map(function(d) { return [scaleX(d.x),scaleY(d.y)].join(","); }).join(" ");})
+    .attr("stroke","black")
+    .attr("stroke-width",2);
